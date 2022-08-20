c9997345320e8bf5cbcc8d3fa997e1d38159bdbd
spsomnath <spsomnath100@gmail.com>
2022-08-20 22:58:45 +0530

    g

---
+++ c/age.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main () {
+    int age;
+    printf("Enter age :");
+    scanf("%d",&age);
+
+    if (age>=18){
+        printf("Adult \n");
+    }
+    else if (age > 13 && age<18)
+    {
+        /* code */
+        printf("Teenage");
+    }
+    else{
+        printf("Child");
+    }
+    
+}
